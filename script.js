e6f9773637850af44b9e88c50b140a106bae93d5
ThomyThom <thomaz.camargo30@gmail.com>
2025-10-19 12:10:02 -0300

    A Grande Inauguração: Versão 1.0

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const password = document.getElementById('login-password').value;
 
             try {
-                const response = await fetch('http://localhost:5000/api/auth/login', {
+                const response = await fetch('/api/auth/login', {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({ email, password })
@@ -99,7 +99,7 @@ document.addEventListener('DOMContentLoaded', () => {
             }
 
             try {
-                const response = await fetch('http://localhost:5000/api/auth/register', {
+                const response = await fetch('/api/auth/register', {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({ name, email, password, confirm_password, school, grade, course, phone, cpf })
